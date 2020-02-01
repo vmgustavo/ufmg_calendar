a60c7b9746d12968e889ca69b770f9f4ddd6e04f
Gustavo Maia <vm.gustavo31@gmail.com>
2020-02-01 12:46:48 -0300

    use bs4 instead of selenium

--- calendar_read.py
+++ calendar_read.py
@@ -1,74 +1,88 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as ec
+from time import sleep
 import pandas as pd
+from bs4 import BeautifulSoup
+import requests
+from datetime import datetime
 import re
-
 import os
 
-# SET CONSTANTS
-feriado_only = True
 
-path = os.path.dirname(os.path.abspath('calendar_read.py'))
+def format_date(s):
+    return datetime.strptime(s, '%Y%m%dT%H%M%SZ').strftime('%d/%m/%Y')
 
-ufmg_calendar_link = 'https://ufmg.br/a-universidade/calendario-academico'
-year = '2019'
-all_month_link_list = [ufmg_calendar_link + '?ano=%s&mes=%s' % (year, month) for month in range(1, 13)]
-driver = webdriver.Chrome(str(path) + '\\chromedriver.exe')
 
-event_info_list = []
-for month_link in all_month_link_list:
-    # month_link = all_month_link_list[0]
-    try:
-        driver.get(month_link)
-        vis_mode = '//*[@id="conteudo"]/div[2]/div[2]/div/a[2]/span[1]'
-        WebDriverWait(driver, 60).until(ec.presence_of_element_located((By.XPATH, vis_mode)))
-        driver.find_element_by_xpath(vis_mode).click()
-    finally:
-        print('SEARCHING FOR ELEMENTS UNDER\n%s' % month_link)
-        all_elements = driver.find_elements_by_xpath('//*[@class]')
+def dump_csv(dframe, filename):
+    # GOOGLE CALENDAR FORMATTING
+    # ['Subject', 'Start Date', 'End Date', 'All Day Event', 'Description']
+    subject_list = list()
+    for elem in dframe['Feriado/Recesso']:
+        if elem:
+            subject_list.append('Feriado/Recesso')
+        else:
+            subject_list.append('Evento (Ler Descrição)')
 
-        event_info_list.extend([(elem.get_attribute('data-info-init-date'),
-                                 elem.get_attribute('data-info-end-date'),
-                                 elem.get_attribute('data-info-location'),
-                                 elem.get_attribute('data-info-title'))
-                                for elem in all_elements if 'calendar__description' in elem.get_attribute('class')])
+    gcal = pd.DataFrame(
+        data=[(subject_list[idx], dframe['init-date'].iloc[idx],
+               dframe['end-date'].iloc[idx], True, dframe['title'].iloc[idx])
+              for idx in range(len(dframe))],
+        columns=['Subject', 'Start Date', 'End Date', 'All Day Event', 'Description'])
 
-# CONVERT DATETIME STRING TO BETTER FORMAT
-df = pd.DataFrame(data=event_info_list, columns=['init-date', 'end-date', 'loc', 'title'])
-df['init-date'] = df['init-date'].apply(
-    lambda x: re.sub(r'(\d{4})(\d{2})(\d{2})T(\d{2})(\d{2})(\d{2})Z', r'\g<3>/\g<2>/\g<1>', x))
-df['end-date'] = df['end-date'].apply(
-    lambda x: re.sub(r'(\d{4})(\d{2})(\d{2})T(\d{2})(\d{2})(\d{2})Z', r'\g<3>/\g<2>/\g<1>', x))
+    # SAVE CSV FILE
+    gcal.to_csv(filename)
+    print(f'Dump file to {os.path.abspath(filename)}')
 
-# FIND IF Feriado OR Recesso
-df = df.join(pd.DataFrame(data=[bool(re.match(r'(\bFeriado\b|\bRecesso\b)', elem)) for elem in df['title']],
-                          columns=['Feriado/Recesso']))
 
-# FILTER LOCATION Belo Horizonte
-df = df[df['loc'].str.contains('Belo Horizonte')].drop('loc', axis=1)
+def main():
+    ufmg_calendar_link = 'https://ufmg.br/a-universidade/calendario-academico'
+    year = datetime.now().year
+    all_month_link_list = [ufmg_calendar_link + '?ano=%s&mes=%s' % (year, month) for month in range(1, 13)]
 
-# DROP DUPLICATES
-df = df.drop_duplicates()
+    event_info = list()
+    for month_link in all_month_link_list:
+        print(month_link)
+        gotpage = False
+        page = None
+        while not gotpage:
+            try:
+                page = requests.get(month_link)
+                gotpage = True
+            except (ConnectionError, requests.exceptions.ConnectionError) as e:
+                stime = 1
+                print(e.__class__.__name__ + f' | Trying again in {stime}s')
+                sleep(stime)
+                gotpage = False
+        soup = BeautifulSoup(page.text, 'html.parser')
+        events = soup.find_all(class_='calendar__description')
+        n_events = len(events)
+        print(f'Found {n_events} events')
+        if not n_events > 0:
+            continue
+        event_info.extend([(elem.get('data-info-init-date'), elem.get('data-info-end-date'),
+                            elem.get('data-info-location'), elem.get('data-info-title'))
+                           for elem in events])
 
-# DROP IF NOT Feriado/Recesso
-if feriado_only:
-    df = df[df['Feriado/Recesso']]
+    # CONVERT DATETIME STRING TO BETTER FORMAT
+    df = pd.DataFrame(data=event_info, columns=['init-date', 'end-date', 'loc', 'title'])
+    df['init-date'] = df['init-date'].apply(format_date)
+    df['end-date'] = df['end-date'].apply(format_date)
 
-# GOOGLE CALENDAR FORMATTING
-# ['Subject', 'Start Date', 'End Date', 'All Day Event', 'Description']
-subject_list = []
-for elem in df['Feriado/Recesso']:
-    if elem:
-        subject_list.append('Feriado/Recesso')
-    else:
-        subject_list.append('Evento (Ler Descrição)')
+    # FIND IF Feriado OR Recesso
+    df = df.join(pd.DataFrame(data=[bool(re.match(r'(\bFeriado\b|\bRecesso\b)', elem)) for elem in df['title']],
+                              columns=['Feriado/Recesso']))
 
-gcal = pd.DataFrame(
-    data=[(subject_list[idx], df['init-date'].iloc[idx], df['end-date'].iloc[idx], True, df['title'].iloc[idx]) for idx
-          in range(len(df))],
-    columns=['Subject', 'Start Date', 'End Date', 'All Day Event', 'Description'])
+    # FILTER LOCATION Belo Horizonte
+    df = df[df['loc'].str.contains('Belo Horizonte')].drop('loc', axis=1)
 
-# SAVE CSV FILE
-gcal.to_csv('ufmg_calendar.csv')
+    # DROP DUPLICATES
+    df = df.drop_duplicates()
+
+    # DROP IF NOT Feriado/Recesso
+    df_feariado = df[df['Feriado/Recesso']]
+    df_evento = df[~df['Feriado/Recesso']]
+
+    dump_csv(df_evento, filename='eventos.csv')
+    dump_csv(df_feariado, filename='feriados.csv')
+
+
+if __name__ == '__main__':
+    main()
